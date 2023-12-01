bca69999c268c39f8cede35dec5d0ecae5df815a
142u1a0327 <94740888+142u1a0327@users.noreply.github.com>
2023-12-01 16:40:11 +0530

    Add files via upload

---
+++ Models/Employeee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRud.Models
+{
+    public class Employeee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ Models/StudentDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRud.Models
+{
+    public class StudentDetails
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Studentname { get; set; }
+        public string Fathername { get; set; }
+        public string Mothername { get; set; }
+    }
+}
